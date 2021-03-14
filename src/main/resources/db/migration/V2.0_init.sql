900f6451628d16b82617d2997ed8561302b68abb
Simonas Liolaitis <simonas.liolaitis@gmail.com>
2021-03-14 13:51:08 +0200

    mentor sql file repair

--- src/main/resources/db/migration/V2.0_init.sql
+++
@@ -1,7 +0,0 @@
-create table lecturer(
-id bigint PRIMARY KEY,
-name varchar(255) not null,
-stream varchar(255) not null
-);
-
-insert into teacher (id, name, stream) value (1, 'Vardenis Pavardenis', 'backend');

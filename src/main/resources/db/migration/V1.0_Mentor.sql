cfa4749620d1ba2ee2dabb7bb228e62ab268479d
Simonas Liolaitis <simonas.liolaitis@gmail.com>
2021-03-14 13:53:21 +0200

    mentor sql file delete

--- src/main/resources/db/migration/V1.0_Mentor.sql
+++
@@ -1,9 +0,0 @@
-create table mentor(
-id bigint PRIMARY KEY,
-name varchar(255) not null,
-stream varchar(255) not null
-);
-
-insert into mentor (id, name, stream) value (1, 'Vardenis Pavardenis', 'backend');
-insert into mentor (id, name, stream) value (2, 'Bevardis Bevardenis', 'frontend');
-insert into mentor (id, name, stream) value (3, 'Nitanas Gausėda', 'QA');

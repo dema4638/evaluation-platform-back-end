28013e97e1a00b5e996be4e1636d1ccf6f989033
Deimena <deimenamar@gmail.com>
2021-03-14 16:20:23 +0200

    Added hibernate configurations

--- src/main/java/com/swedbank/academy/evaluationplatform/student/Student.java
+++ src/main/java/com/swedbank/academy/evaluationplatform/student/Student.java
@@ -1,5 +1,8 @@
 package com.swedbank.academy.evaluationplatform.student;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -7,6 +10,8 @@ import javax.persistence.Table;
 
 @Entity
 @Table
+@Getter
+@Setter
 public class Student {
 
     @Id
@@ -17,7 +22,6 @@ public class Student {
 
     @Column(name = "image")
     private String image;
-
 
     public Student() {
     }

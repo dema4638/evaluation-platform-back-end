75d88561eba0b41e802ffcb999843bd5fcadeeb3
Simonas Liolaitis <simonas.liolaitis@gmail.com>
2021-03-29 15:54:24 +0300

    New deleteStudent method

--- src/main/java/com/swedbank/academy/evaluationplatform/student/StudentRepository.java
+++ src/main/java/com/swedbank/academy/evaluationplatform/student/StudentRepository.java
@@ -1,9 +1,16 @@
 package com.swedbank.academy.evaluationplatform.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
+import javax.transaction.Transactional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Evaluation evaluation WHERE evaluation.student.id = ?1")
+    void deleteStudentEvaluations(long studentId);
 
 }
